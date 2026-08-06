9e2073c57876ab12a8533d1e40545e13c7afd978
Vadim Miroshnikov <zamiroshvaz2000@gmail.com>
2026-08-06 19:26:36 +0300

    hw4 modified

--- Homeworks/js/fix_names.js
+++ Homeworks/js/fix_names.js
@@ -2,19 +2,17 @@
 let userAge = 25; // Назви змінних повинні мати точний зміст
 let name = 'Anna'; // Назви змінних повинні мати точний зміст
 let userName = 'Bob'; // Назва змінної не може починатися з цифри
-let userage = 30; // - в JS це математичний оператор тому його не можна використовувати в назві змінної
+let userAage = 30; // - в JS це математичний оператор тому його не можна використовувати в назві змінної
 let userFirstName = 'Charlie'; // Воно б і так працювало, але так не пишуть код, на початку літера повинна бути маленька
-let username = 'Dmytro'; // Міднародний стандарт це англійська мова
+let userNname = 'Dmytro'; // Міднародний стандарт це англійська мова
 let balance = 100000; // Що таке а1?
 let userData = { age: 20 }; // Можна зробити букву d великою
 
 console.log("User Age:", userAge)
 console.log("Name:", name)
 console.log("User name:", userName)
-console.log("User age:", userage)
+console.log("User age:", userAage)
 console.log("User first name:", userFirstName)
-console.log("User name:", username)
+console.log("User name:", userNname)
 console.log("Balance:", balance)
 console.log("User data:", userData)
-
-// Через повтори назв змінних деякі з них написані не як camelCase, якщо це проблема то я можу їх переписати
