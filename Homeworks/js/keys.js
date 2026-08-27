3620d2296b0e955152925568be058b18a5d39172
Vadim Miroshnikov <zamiroshvaz2000@gmail.com>
2026-08-27 16:26:26 +0300

    hw16 added

---
+++ Homeworks/js/keys.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function generateKey(length, characters) {
+    let key = "";
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        key += characters[randomIndex];
+    }
+    
+    return key;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+console.log(generateKey(16, characters));
